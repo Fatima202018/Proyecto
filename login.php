c68275296d627a13a261895536917f31d89e05cf
Fatima202018 <73596272+Fatima202018@users.noreply.github.com>
2021-08-12 18:01:09 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,16 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Login</title>
-     
-    <link rel="stylesheet" href="/style.css"> 
-</head>
-<body>
-   <div id="login">
-  <form>
-    <h1>Sign In</h1>
-    <input type="text" placeholder="Nombre Usuario">
-    <input type="password" placeholder="Contraseña">
-    <button>INGRESAR</button>
-  </form>
-</div>
-</body>
+<?php
+    session_start();
+    $user = $_SESSION["user"];
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Administrador</title>
+    </head>
+    <body>
+        <h3><b>Bienvenido/a: <?php echo $user; ?> </b></h3>
+    </body>
 </html>
